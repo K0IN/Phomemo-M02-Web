131e4f70ddc140852c9d6f2a8a16c831a6b90114
K0IN <K0IN@users.noreply.github.com>
2025-06-01 22:06:44 +0000

    deploy: 2ec75ae2ac7b8c2464b4aed453a7274b1deab915

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Phomemo-M02-Web/sw.js', { scope: '/Phomemo-M02-Web/' })})}
