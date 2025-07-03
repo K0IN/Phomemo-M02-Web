95debcde5d93e8c1b338d103aca4ad1cc09ca902
K0IN <thisk0in@gmail.com>
2025-07-03 02:11:16 +0200

    Fix share target code

--- src/serviceworker.ts
+++ src/serviceworker.ts
@@ -11,10 +11,40 @@ clientsClaim()
 cleanupOutdatedCaches()
 precacheAndRoute(self.__WB_MANIFEST)
 
+function sleepMs(time: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+async function getClientById(clientId: string, abortSignal: AbortSignal): Promise<Client | undefined> {
+    while (!abortSignal.aborted) {
+        const client = await self.clients.get(clientId);
+        if (client) {
+            return client;
+        }
+        await sleepMs(250);
+    }
+    return undefined;
+}
+
+async function sendImageToWebApp(file: File, clientId: string) {
+    // todo handle clientid == '' see https://developer.mozilla.org/en-US/docs/Web/API/FetchEvent/resultingClientId
+    const abort = AbortSignal.timeout(15_000);
+    const client = await getClientById(clientId, abort);
+    if (!client) {
+        console.warn('Client not found:', clientId);
+        return;
+    }
+
+    console.log('Sending image to client:', client);
+    client.postMessage({ type: 'image_shared', file });
+}
 
 
 async function handleShareTarget(request: Request, event: FetchEvent) {
+    console.log('Handling share target request:', request.url, event);
     const formData = await request.formData();
+
+
 
     const file = formData.get('image');
     if (!(file instanceof File)) {
@@ -26,22 +56,11 @@ async function handleShareTarget(request: Request, event: FetchEvent) {
     }
 
     console.log('Received image file:', file.name, file.size, file.type);
-
-    event.waitUntil(self.clients.matchAll({ includeUncontrolled: true, type: 'window' }).then(clients => {
-
-        return Promise.all(clients.map(async client => {
-            await new Promise(resolve => setTimeout(resolve, 5_000)); // Wait for 1 second to ensure the client is ready
-
-            if (client.visibilityState === 'visible') {
-                await client.focus();
-            }
-            return client.postMessage({ type: 'image_shared', file });
-        }));
-    }));
-
+    event.waitUntil(sendImageToWebApp(file, event.resultingClientId));
     return Response.redirect(request.url.replace('/share-target', ''), 303);// https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Manifest/Reference/share_target
 }
 
 registerRoute(
     ({ request }) => request.url.endsWith('/share-target'),
-    async ({ event, request }) => handleShareTarget(request, event as FetchEvent), 'POST');
+    async ({ event, request }) => handleShareTarget(request, event as FetchEvent),
+    'POST');
